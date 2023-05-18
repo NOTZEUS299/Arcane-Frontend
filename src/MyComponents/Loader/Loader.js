6ae8cb2e22d5e45a15fcf60d91368111a8d34917
Darshan-299 <113411879+Darshan-299@users.noreply.github.com>
2023-05-18 14:09:47 +0530

    loader update

--- src/MyComponents/Loader/Loader.js
+++ src/MyComponents/Loader/Loader.js
@@ -1,16 +1,21 @@
 import React, { useEffect } from "react";
 import "./loader.css";
+import A from "../../images/a.png";
+import R from "../../images/r.png";
+import K from "../../images/k.png";
+import N from "../../images/n.png";
+import E from "../../images/e.png";
 
 export const Loader = () => {
   return (
     <div>
       <div className="loader-main-container">
-        <img src="a.png" />
-        <img src="r.png" style={{ animationDelay: "0.1s" }} />
-        <img src="k.png" style={{ animationDelay: "0.2s" }} />
-        <img src="a.png" style={{ animationDelay: "0.3s" }} />
-        <img src="n.png" style={{ animationDelay: "0.4s" }} />
-        <img src="e.png" style={{ animationDelay: "0.5s" }} />
+        <img src={A} />
+        <img src={R} style={{ animationDelay: "0.1s" }} />
+        <img src={K} style={{ animationDelay: "0.2s" }} />
+        <img src={A} style={{ animationDelay: "0.3s" }} />
+        <img src={N} style={{ animationDelay: "0.4s" }} />
+        <img src={E} style={{ animationDelay: "0.5s" }} />
       </div>
     </div>
   );
